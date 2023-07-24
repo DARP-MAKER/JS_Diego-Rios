543bec96a3f3fe65421a0f64ab1f383accab7950
DARP-MAKER <ingenierodiegorios@gmail.com>
2023-07-24 18:47:12 -0400

    PreEntrega1 finalizada

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
-
-//function primeNumber(num1)
-//if (num1 % 1 === 0 && num1/num1 === 0) {
-  //  return "primo";
-  //} else {
-   // return "no primo";
-  //}
-
-//let numero = parseInt(prompt("ingrese numero"))
-//let resultado = primeNumber(numero);
-
 function primeNumber(num1) {
   if (num1 === 1) {
-    return "no primo";
+    return "el numero ingresado no es primo";
   } else if (num1 === 2) {
-    return "primo";
+    return "el numero ingresado es primo";
   } else {
     for (let i = 2; i < num1; i++) {
       if (num1 % i === 0) {
-        return "no primo";
+        return "el numero ingresado no es primo";
       }
     }
-    return "primo";
+    return "el numero ingresado es primo";
   }
 }
 
-let numero = parseInt(prompt("Ingrese un número"));
-let resultado = primeNumber(numero);
-alert(resultado);
+let continuar = true;
+
+while (continuar) {
+  let numero = parseInt(prompt("Ingrese un número"));
+  let resultado = primeNumber(numero);
+  alert(resultado);
+
+  let respuesta = prompt("¿Desea ingresar otro número? (s/n)");
+
+  while (respuesta.toLowerCase() !== "s" && respuesta.toLowerCase() !== "n") {
+    alert("Ingrese solo 's' o 'n'.");
+    respuesta = prompt("¿Desea ingresar otro número? (s/n)");
+  }
+
+  continuar = respuesta.toLowerCase() === "s";
+}
 
 
 
